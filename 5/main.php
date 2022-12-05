45ccb8de7e90d32a7ac39e849ed2b86a705f75f5
Pascal Pirschel <pirschel@lamapoll.de>
2022-12-05 13:24:36 +0100

    Finished day 5

---
+++ 5/main.php
@@ -0,0 +1,35 @@
+<?php
+
+[$rawBoxLines, $rawMoveLines] = explode("\n\n", trim(file_get_contents(__DIR__ . '/input.txt')));
+
+$boxLines = array_reverse(explode("\n", $rawBoxLines));
+$towers = array_fill_keys(array_filter(explode(' ', array_splice($boxLines, 0, 1)[0])), []);
+
+foreach ($boxLines as $line){
+    for ($index = 1; $index <= count($towers); $index++){
+        $box = substr($line, ($index * 4) - 3, 1);
+        if (!trim($box)) continue;
+
+        $towers[$index][] = $box;
+    }
+}
+
+$moveLines = explode("\n", $rawMoveLines);
+foreach ($moveLines as $moveLine) {
+    $params = explode(' ', $moveLine);
+    [$amount, $from, $to] = [$params[1], $params[3], $params[5]];
+
+    while ($amount > 0) {
+        $amount--;
+
+        $box = array_pop($towers[$from]);
+        $towers[$to][] = $box;
+    }
+}
+
+// Print highest boxes
+foreach ($towers as $tower) {
+    $last = $tower[array_key_last($tower)];
+    echo $last;
+}
+echo "\n";
